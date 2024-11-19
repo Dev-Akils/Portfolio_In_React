4db33830bb365399f255baf83f7934d2d5d89002
Maharajan-hub <maharaja182409@gmail.com>
2024-11-19 14:09:03 +0530

    initial

--- src/Components/Experience/Experience.js
+++ src/Components/Experience/Experience.js
@@ -5,7 +5,9 @@ function Experience() {
     <div className='w-full h-full mx-auto container text-center justify-center items-center'>
     <h2 className='text-3xl font-bold py-3 mb-3'>Experience</h2>
     <div className='p-10 md:p-16 text-lg mx-10 shadow-lg md:mx-20 shadow-black bg-purple-50 rounded'>
-        <p>Kairaa Tech Serve Pvt.Ltd -10 Months Work Experience as Developer in React JS</p>
+    <p className='font-bold'>Total Work Experience: 1.3 Years</p>
+       <p className='text-start'>Junior React.js Developer at <span className='font-bold'>Kairaa Tech Serve Pvt. Ltd</span>: 11 months of hands-on experience in building responsive and dynamic web applications using React.js.</p>
+        <p className='text-start'>Frontend Development Intern at <span className='font-bold'> Sparkz Tech Pvt. Ltd</span>: 4 months of intensive training and practical experience in modern frontend technologies.</p>
     </div>
     
     
